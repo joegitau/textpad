77edbbf9e8eb3be2165929e0c7c4068db50c87a8
Joe Karanka <joekaranka@Joes-iMac.local>
2020-03-07 22:16:48 +0200

    basic scaffolding + styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { BaseComponent } from "./base/base.component";
+import { NotesListComponent } from "./notes-list/notes-list.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: BaseComponent,
+    children: [
+      {
+        path: "",
+        component: NotesListComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotesListComponent } from './notes-list/notes-list.component';
+import { BaseComponent } from './base/base.component';
+import { NoteComponent } from './note/note.component';
+import { NoteDetailsComponent } from './note-details/note-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotesListComponent,
+    BaseComponent,
+    NoteComponent,
+    NoteDetailsComponent
   ],
   imports: [
     BrowserModule,
