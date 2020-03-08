474d5fc444f07c2f140582aa2df3877f1a6455af
Joe <Joe@Joe-Air.local>
2020-03-08 16:54:49 +0200

    refactored note-component rendering

--- src/app/note/note.component.ts
+++ src/app/note/note.component.ts
@@ -1,6 +1,6 @@
 import {
   Component,
-  OnInit,
+  AfterViewInit,
   Renderer2,
   ViewChild,
   ElementRef
@@ -11,15 +11,15 @@ import {
   templateUrl: "./note.component.html",
   styleUrls: ["./note.component.scss"]
 })
-export class NoteComponent implements OnInit {
+export class NoteComponent implements AfterViewInit {
   @ViewChild("truncator") truncator: ElementRef<HTMLElement>;
   @ViewChild("noteText") noteText: ElementRef<HTMLElement>;
 
   constructor(private renderer: Renderer2) {}
 
-  ngOnInit(): void {
-    let style = window.getComputedStyle(this.noteText.nativeElement, null);
-    let viewableHeight = parseInt(style.getPropertyValue("height"), 10);
+  ngAfterViewInit(): void {
+    const style = window.getComputedStyle(this.noteText.nativeElement, null);
+    const viewableHeight = parseInt(style.getPropertyValue("height"), 10);
 
     if (this.noteText.nativeElement.scrollHeight > viewableHeight) {
       this.renderer.setStyle(this.truncator.nativeElement, "display", "block");
