ab9485b2784f297f6ce8ce8d439ac3ea5f60eb40
Joe <Joe@Joe-Air.local>
2020-03-08 22:12:17 +0200

    create note creation form

--- src/app/note-details/note-details.component.ts
+++ src/app/note-details/note-details.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+
+import { INote } from "../shared/note.module";
 
 @Component({
-  selector: 'app-note-details',
-  templateUrl: './note-details.component.html',
-  styleUrls: ['./note-details.component.scss']
+  selector: "app-note-details",
+  templateUrl: "./note-details.component.html",
+  styleUrls: ["./note-details.component.scss"]
 })
 export class NoteDetailsComponent implements OnInit {
+  noteForm: FormGroup;
+  note: INote;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.noteForm = this.fb.group({
+      noteTitle: ["", [Validators.required, Validators.minLength(3)]],
+      noteBody: ["", Validators.required]
+    });
   }
 
+  createNote() {
+    console.log(this.noteForm);
+    console.log("Saved", JSON.stringify(this.noteForm.value));
+  }
 }
